f458aec75d1ddfb53e881919e4d15444bf3fb339
Devon Walsh <devonwalsh@gmail.com>
2021-03-19 22:14:49 -0400

    commit tree 1b8f900b376a7826a57ed152d40ea7c436e9a2fb

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
-function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+const configObj = {
+  mode: "cors",
+  headers: {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'},
+  }
   
-}
+function fetchBooks() {
+  return fetch('https://anapioficeandfire.com/api/books', configObj)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(json) {
+    const books = json;
+    renderBooks(books);
+  })
+} 
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
-}
+    const main = document.querySelector('main');
+    books.forEach(book => {
+      const h2 = document.createElement('h2');
+      h2.innerHTML = book.name;
+      main.appendChild(h2);
+    });
+  }
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
